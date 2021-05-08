52962a5b265eb20a61fe05ae8972663e9151aaab
mini18do <83375590+mini18do@users.noreply.github.com>
2021-05-08 20:44:36 +0100

    Update DO-Auto-Move.user.js

--- DO-Auto-Move.user.js
+++ DO-Auto-Move.user.js
@@ -15,27 +15,13 @@ if (document.querySelectorAll(".top_positions").length > 0) {
       let id = e.children[2].children[1].id;
       let age = parseInt(e.children[4].textContent);
       age < 19
-        ? xmlHttp(
-            "POST",
-            "https://www.dugout-online.com/players/details/playerID/" + id,
-            "application/x-www-form-urlencoded",
-            postData,
-            function () {
-              e.remove();
-            }
-          )
+        ? fetch("https://www.dugout-online.com/players/details/playerID/" + id,{method: "POST",headers: { "Content-Type": "application/x-www-form-urlencoded" },body: postData,})
+            //.then((response) => console.log(response))
+            .then(() => e.remove())
         : null;
     });
   }
-  function xmlHttp(method, url, contenType, postData, callBack) {
-    fetch(url, {
-      method: method,
-      headers: { "Content-Type": contenType },
-      body: postData,
-    })
-      .then((response) => console.log(response))
-      .then((data) => callBack(data));
-  }
+
   let newcell = document
     .querySelector(".compare_players_wrapper tr")
     .insertCell(4);
@@ -49,4 +35,4 @@ if (document.querySelectorAll(".top_positions").length > 0) {
   document.querySelector("#first1") != null
     ? ((postData = "movetoyouth=1"), (newbutton.value = "move to youth"))
     : ((postData = "moveto1st=1"), (newbutton.value = "move to 1st"));
-}
+};
