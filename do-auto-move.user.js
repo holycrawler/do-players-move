6b76b439e39a13383683811cb2068722553874d4
holycrawler <happyxzt@gmail.com>
2021-10-05 17:31:07 +0100

    less stupid

--- do-auto-move.user.js
+++ do-auto-move.user.js
@@ -1,24 +1,28 @@
 // ==UserScript==
 // @name    DO-Auto-Move
-// @version 3.1
+// @version 3.2
 // @description auto move players (it works 60% of the time every time)
 // @author  holycrawler
 // @include *dugout-online.com/players/*
 // @update https://github.com/holycrawler/do-players-move/raw/main/DO-Auto-Move.user.js
 // ==/UserScript==
 
-if (document.querySelectorAll(".top_positions").length > 0) {
+if (document.querySelector(".top_positions") != null) {
   let postData;
   function movePlayers() {
     let table = document.querySelectorAll("tr[class*='matches_row']");
     table.forEach(function (e) {
       let id = e.children[2].children[1].id;
       let age = parseInt(e.children[4].textContent);
-      age < 19
-        ? fetch("https://www.dugout-online.com/players/details/playerID/" + id,{method: "POST",headers: { "Content-Type": "application/x-www-form-urlencoded" },body: postData,})
-            //.then((response) => console.log(response))
-            .then(() => e.remove())
-        : null;
+      if (age < 19) {
+        fetch("https://www.dugout-online.com/players/details/playerID/" + id, {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: postData,
+        })
+          //.then((response) => console.log(response))
+          .then(() => e.remove());
+      }
     });
   }
 
@@ -32,7 +36,8 @@ if (document.querySelectorAll(".top_positions").length > 0) {
   newbutton.type = "button";
   newcell.append(newbutton);
   newbutton.addEventListener("click", movePlayers, true);
-  document.querySelector("#first1") != null
-    ? ((postData = "movetoyouth=1"), (newbutton.value = "move to youth"))
-    : ((postData = "moveto1st=1"), (newbutton.value = "move to 1st"));
-};
+  [postData, newbutton.value] =
+    document.querySelector("#first1") != null
+      ? ["movetoyouth=1", "move to youth"]
+      : ["moveto1st=1", "move to 1st"];
+}
