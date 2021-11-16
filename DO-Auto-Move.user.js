24c7d614cd72b6ee8868ab598ec17595f61ac452
holycrawler <happyxzt@gmail.com>
2021-11-16 23:11:28 +0100

    spreadsheet view support

--- DO-Auto-Move.user.js
+++ DO-Auto-Move.user.js
@@ -7,11 +7,12 @@
 // @update https://github.com/holycrawler/do-players-move/raw/main/DO-Auto-Move.user.js
 // ==/UserScript==
 
-if (document.querySelector(".top_positions") != null) {
+(() => {
+  if (!document.querySelector(".top_positions")) return;
   const isSpreadSheet = !document.querySelector("#spread2").className.match("tab_off");
   const n = isSpreadSheet ? 1 : 0;
   let postData;
-  function movePlayers() {
+  const movePlayers = () => {
     const table = document.querySelectorAll("tr[class*='matches_row']");
     table.forEach((e) => {
       const url = e.querySelector("a").href;
@@ -26,16 +27,14 @@ if (document.querySelector(".top_positions") != null) {
           .then(() => e.remove());
       }
     });
-  }
-
+  };
   const newcell = document.querySelector(".compare_players_wrapper tr").insertCell();
-  const newbutton = document.createElement("input");
   if (!isSpreadSheet) newcell.style.width = "39%";
+  const newbutton = document.createElement("input");
   newbutton.type = "button";
   newbutton.style.float = "right";
   newcell.append(newbutton);
   newbutton.addEventListener("click", movePlayers);
-
   const isFirst = isSpreadSheet ? goalkeepers.youth.value === "0" : document.querySelector("#first1") != null;
   [postData, newbutton.value] = isFirst ? ["movetoyouth=1", "move to youth"] : ["moveto1st=1", "move to 1st"];
-}
+})();
